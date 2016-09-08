ad6096f97e1a17c534c681dce826991ff065009e
Ernie Miller <ernie@ernie.io>
2016-09-08 16:04:49 -0400

    Updates to work with Rails 4.x

--- lib/dieter/engine.rb
+++ lib/dieter/engine.rb
@@ -6,8 +6,19 @@ require 'dieter/locale_template'
 
 module Dieter
   class Engine < ::Rails::Engine
-    initializer 'dieter.locale_template', :after => 'sprockets.environment' do |app|
-      app.assets.register_engine '.locale', LocaleTemplate
+    initializer "dieter.locale_template", :group => :all do |app|
+      app.config.assets.configure do |sprockets_env|
+        if sprockets_env.respond_to?(:register_transformer)
+          sprockets_env.register_mime_type 'application/vnd.dieter.locale+text', extensions: ['.locale']
+          sprockets_env.register_transformer 'application/vnd.dieter.locale+text', 'application/javascript', LocaleTemplate
+        end
+
+        if sprockets_env.respond_to?(:register_engine)
+          args = ['.locale', LocaleTemplate]
+          args << { silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+          sprockets_env.register_engine(*args)
+        end
+      end
     end
 
     initializer 'dieter.helpers' do
